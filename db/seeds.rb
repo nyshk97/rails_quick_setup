b4a84565136f2fc7f63af9376bdfce6acf5fbda5
tsubasa namatame <nyshk97@gmail.com>
2019-12-26 11:36:11 +0900

    Create data for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Test.create!(
+  [
+    {
+      name: 'テスト太郎'
+    },
+    {
+      name: 'テスト花子'
+    }
+  ]
+)
