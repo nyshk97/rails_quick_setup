f17cfb1d059fae3a26d97bad22d0c3686aa676f9
tsubasa namatame <nyshk97@gmail.com>
2019-12-27 20:29:27 +0900

    Add a name space

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,3 @@
+class Admin::ApplicationController < ActionController::Base
+  layout 'admin/layouts/application'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'static_pages/index'
+  end
   root 'tests#index'
   resources :tests
 end
